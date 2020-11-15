f10d9dd2fae49a019c655cc429c6aa8c4407a7d2
TomoyaHirano <tomoya.hirano.th@gmail.com>
2020-11-15 15:21:06 +0900

    implement client side

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,156 +1,91 @@
-/******************************************************************************
- *                          Fetch and display users
- ******************************************************************************/
-
-console.log("------------------------------------")
-displayUsers();
-
-
-function displayUsers() {
-    httpGet('/api/users/all')
-        .then(response => response.json())
-        .then((response) => {
-            var allUsers = response.users;
-            // Empty the anchor
-            var allUsersAnchor = document.getElementById('all-users-anchor');
-            allUsersAnchor.innerHTML = '';
-            // Append users to anchor
-            allUsers.forEach((user) => {
-                allUsersAnchor.innerHTML += getUserDisplayEle(user);
-            });
-        });
-};
-
-
-function getUserDisplayEle(user) {
-    return `<div class="user-display-ele">
-
-        <div class="normal-view">
-            <div>Name: ${user.name}</div>
-            <div>Email: ${user.email}</div>
-            <button class="edit-user-btn" data-user-id="${user.id}">
-                Edit
-            </button>
-            <button class="delete-user-btn" data-user-id="${user.id}">
-                Delete
-            </button>
-        </div>
-        
-        <div class="edit-view">
-            <div>
-                Name: <input class="name-edit-input" value="${user.name}">
-            </div>
-            <div>
-                Email: <input class="email-edit-input" value="${user.email}">
-            </div>
-            <button class="submit-edit-btn" data-user-id="${user.id}">
-                Submit
-            </button>
-            <button class="cancel-edit-btn" data-user-id="${user.id}">
-                Cancel
-            </button>
-        </div>
-    </div>`;
-}
-
-
-/******************************************************************************
- *                        Add, Edit, and Delete Users
- ******************************************************************************/
-
-document.addEventListener('click', function (event) {
-    event.preventDefault();
-    var ele = event.target;
-    if (ele.matches('#add-user-btn')) {
-        addUser();
-    } else if (ele.matches('.edit-user-btn')) {
-        showEditView(ele.parentNode.parentNode);
-    } else if (ele.matches('.cancel-edit-btn')) {
-        cancelEdit(ele.parentNode.parentNode);
-    } else if (ele.matches('.submit-edit-btn')) {
-        submitEdit(ele);
-    } else if (ele.matches('.delete-user-btn')) {
-        deleteUser(ele);
+async function getForecastWeather(){
+    path = '/api/weather/forecast/';
+    if(document.getElementById("id").value!=""){
+        path += document.getElementById("id").value;
     }
-}, false)
-
-
-function addUser() {
-    var nameInput = document.getElementById('name-input');
-    var emailInput = document.getElementById('email-input');
-    var data = {
-        user: {
-            name: nameInput.value,
-            email: emailInput.value
-        },
-    };
-    httpPost('/api/users/add', data)
-        .then(() => {
-            displayUsers();
-        })
+    promise = await httpGet(path)
+    json = await promise.json()
+    document.getElementById("apioutput").innerHTML = JSON.stringify(json)
 }
 
-
-function showEditView(userEle) {
-    var normalView = userEle.getElementsByClassName('normal-view')[0];
-    var editView = userEle.getElementsByClassName('edit-view')[0];
-    normalView.style.display = 'none';
-    editView.style.display = 'block';
+async function postForecastWeather(){
+    path = "/api/weather/forecast/"
+    if(document.getElementById("id").value!=""){
+        alert("DO NOT Specify id")
+        return
+    }
+    data = {
+        //do not specify id when you do post
+        "year" : parseInt(document.getElementById("year").value),
+        "month" : parseInt(document.getElementById("month").value),
+        "day" :parseInt(document.getElementById("day").value),
+        "hour" : parseInt(document.getElementById("hour").value),
+        "minute" : parseInt(document.getElementById("minute").value),
+        "place_id"  : parseInt(document.getElementById("place_id").value),
+        "rainfall" : parseInt(document.getElementById("rainfall").value)
+    }
+    promise = await httpPost(path, data)
+    json = await promise.json()
+    document.getElementById("apioutput").innerHTML = JSON.stringify(json)
 }
 
-
-function cancelEdit(userEle) {
-    var normalView = userEle.getElementsByClassName('normal-view')[0];
-    var editView = userEle.getElementsByClassName('edit-view')[0];
-    normalView.style.display = 'block';
-    editView.style.display = 'none';
+async function patchForecastWeather(){
+    path = "/api/weather/forecast/"
+    data = {}
+    if(document.getElementById("id").value==""){
+        alert("Please Specify id")
+        return
+    }
+    path += document.getElementById("id").value;
+    if(document.getElementById("year").value!=""){
+        data.year = parseInt(document.getElementById("year").value)
+    }
+    if(document.getElementById("month").value!=""){
+        data.month = parseInt(document.getElementById("month").value)
+    }
+    if(document.getElementById("day").value!=""){
+        data.day = parseInt(document.getElementById("day").value)
+    }
+    if(document.getElementById("hour").value!=""){
+        data.hour = parseInt(document.getElementById("hour").value)
+    }
+    if(document.getElementById("minute").value!=""){
+        data.minute = parseInt(document.getElementById("minute").value)
+    }
+    if(document.getElementById("place_id").value!=""){
+        data.place_id = parseInt(document.getElementById("place_id").value)
+    }
+    if(document.getElementById("rainfall").value!=""){
+        data.rainfall = parseInt(document.getElementById("rainfall").value)
+    }
+    promise = await httpPatch(path, data)
+    json = await promise.json()
+    document.getElementById("apioutput").innerHTML = JSON.stringify(json)
 }
 
-
-function submitEdit(ele) {
-    var userEle = ele.parentNode.parentNode;
-    var nameInput = userEle.getElementsByClassName('name-edit-input')[0];
-    var emailInput = userEle.getElementsByClassName('email-edit-input')[0];
-    var id = ele.getAttribute('data-user-id');
-    var data = {
-        user: {
-            name: nameInput.value,
-            email: emailInput.value,
-            id: id
-        }
-    };
-	httpPut('/api/users/update', data)
-        .then(() => {
-            displayUsers();
-        })
+async function deleteForecastWeather(){
+    if(document.getElementById("id").value==""){
+        alert("Please Specify id")
+        return
+    }
+    path = '/api/weather/forecast/' + document.getElementById("id").value;
+    promise = await httpDelete(path)
+    json = await promise.json()
+    document.getElementById("apioutput").innerHTML = JSON.stringify(json)
 }
 
-
-function deleteUser(ele) {
-    var id = ele.getAttribute('data-user-id');
-	httpDelete('/api/users/delete/' + id)
-        .then(() => {
-            displayUsers();
-        })
-}
-
-
-async function httpGet(path) {
+function httpGet(path) {
     console.log("httpGet")
-    res = await fetch(path, getOptions('GET'))
-    json = await res.json()
-    document.getElementById("apioutput").innerHTML = JSON.stringify(json.forecastWeathers[0])
+    return fetch(path, getOptions('GET'))
 }
-
 
 function httpPost(path, data) {
     console.log("httpPost")
     return fetch(path, getOptions('POST', data));
 }
 
-
-function httpPut(path, data) {
-    return fetch(path, getOptions('PUT', data));
+function httpPatch(path, data) {
+    return fetch(path, getOptions('PATCH', data));
 }
 
 
